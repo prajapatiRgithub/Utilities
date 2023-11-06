dce253fcde834e718cf8c2e56e04249db4f4ab97
prajapatiRavi <prajapatiravi.shivinfotech@gmail.com>
2023-11-06 11:52:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+    limit: '50mb',
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is running.. :${port}`);
+});
